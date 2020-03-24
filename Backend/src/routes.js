a14e622a43d23e6ace74bd969705d045ffe91cbe
carlosziegler <carlos.ziegler@gmail.com>
2020-03-24 19:28:24 +0100

    #2
    Create a ProfileController, SessionController

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -2,9 +2,15 @@ const express = require('express')
 const routes = express.Router()
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileConroller')
+const SessionsController = require('./controllers/SessionConroller')
+
+routes.post('/sessions', SessionsController.store)
 
 routes.post('/users', OngController.store)
 routes.get('/users', OngController.index)
+
+routes.get('/profile', ProfileController.index)
 
 routes.post('/incidents', IncidentController.store)
 routes.get('/incidents', IncidentController.index)
