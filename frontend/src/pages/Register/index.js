cbb02c434c1bef3250f2f9596b2aa3b883697f9d
carlosziegler <carlos.ziegler@gmail.com>
2020-03-25 19:55:07 +0100

    #3
    Register page connect a API, Bug in Backend: Route/ ongs

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom'
+import axios from 'axios';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api'
 
 import './styles.css'
 import logoImg from '../../assets/logo.svg'
 
 function Register() {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [whatsapp, setWhats] = useState('')
+    const [city, setCity] = useState('')
+    const [uf, setUF] = useState('')
+
+    
+
+    async function handleRegister(e) {
+       e.preventDefault()
+       const data = {
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        }
+        
+
+        try {
+            const response = await api.post('/ongs', data)
+            alert(`Your access ID : ${response.data.id}`)
+
+        } catch (error) {
+            alert(`Error for register, try again`)
+        }
+        
+    }
+
     return (
        <div className="register-container">
           <div className="content">
@@ -18,16 +51,45 @@ function Register() {
                        Register
                 </Link>
               </section>
-              <form>
-                <input type="text" placeholder="NGO Name"/>
-                <input type="email" placeholder="Email"/>
-                <input type="text" placeholder="Whatsapp"/>
+              <form onSubmit={handleRegister}>
+                <input 
+                type="text"
+                placeholder="NGO Name" 
+                value={name} 
+                onChange={e => {setName(e.target.value)}}
+                />
+                <input 
+                type="email" 
+                placeholder="Email" 
+                value={email} 
+                onChange={e => {setEmail(e.target.value)}}
+                />
+                <input 
+                type="text" 
+                placeholder="Whatsapp" 
+                value={whatsapp} 
+                onChange={e => {setWhats(e.target.value)}}
+                />
                 <div className="input-group">
-                    <input type="text" placeholder="City"/>
-                    <input type="text" placeholder="UF" style={{width:80}}/>
+                    <input 
+                    type="text" 
+                    placeholder="City" 
+                    value={city} 
+                    onChange={e => {setCity(e.target.value)}}
+                    />
+                    <input 
+                    type="text" 
+                    placeholder="UF" 
+                    value={uf} 
+                    onChange={e => {setUF(e.target.value)}}style={{width:80}}
+                    />
                 </div>
 
-                <button className="button" type="submit">Register</button>
+                <button 
+                className="button" 
+                type="submit">
+                    Register
+                </button>
                 
               </form>
           </div>
