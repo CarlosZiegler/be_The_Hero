866938ec41846d498c392b3b1f40a04a0b23f431
carlosziegler <carlos.ziegler@gmail.com>
2020-03-25 18:04:36 +0100

    #3
    Create a Profile Page

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,14 +1,93 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
-import { FiArrowLeft } from 'react-icons/fi';
+import { FiPower, FiTrash } from 'react-icons/fi';
 
 import './styles.css'
 import logoImg from '../../assets/logo.svg'
 
 function Profile() {
     return (
-       <div className="register-container">
-          <h1>Profile</h1>
+       <div className="profile-container">
+          <header className="">
+            <img src={logoImg} alt="Logo"/>
+            <span className="">Bem Vindo, OnGNAME</span>
+            
+            <Link className="button" to="/incidents/new">
+              Create a new Incident
+            </Link>
+
+            <button type="button" > 
+              <FiPower size={16}  color="#e02041"/>
+            </button>
+          </header>
+          <h1>Incidents registered </h1>
+
+
+          <ul>
+            <li>
+              <strong>Incident: </strong>
+              <p>Incident Test</p>
+              
+              <strong>Description: </strong>
+              <p>Description Test</p>
+              
+              <strong>Value: </strong>
+              <p>$100,00</p>
+
+              <button type="button">
+                <FiTrash size={16}  color="#a8a8b3"/>
+              </button>
+
+            </li>
+
+            <li>
+              <strong>Incident: </strong>
+              <p>Incident Test</p>
+              
+              <strong>Description: </strong>
+              <p>Description Test</p>
+              
+              <strong>Value: </strong>
+              <p>$100,00</p>
+
+              <button type="button">
+                <FiTrash size={16}  color="#a8a8b3"/>
+              </button>
+
+            </li>
+
+            <li>
+              <strong>Incident: </strong>
+              <p>Incident Test</p>
+              
+              <strong>Description: </strong>
+              <p>Description Test</p>
+              
+              <strong>Value: </strong>
+              <p>$100,00</p>
+
+              <button type="button">
+                <FiTrash size={16}  color="#a8a8b3"/>
+              </button>
+
+            </li>
+            <li>
+              <strong>Incident: </strong>
+              <p>Incident Test</p>
+              
+              <strong>Description: </strong>
+              <p>Description Test</p>
+              
+              <strong>Value: </strong>
+              <p>$100,00</p>
+
+              <button type="button">
+                <FiTrash size={16}  color="#a8a8b3"/>
+              </button>
+
+            </li>
+          </ul>
+
        </div> 
     );
 }
