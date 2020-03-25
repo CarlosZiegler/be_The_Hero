822fef20c75da49937d276fc65739507ce26521d
carlosziegler <carlos.ziegler@gmail.com>
2020-03-25 17:28:57 +0100

    #3
    Create a Register Page

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './styles.css'
+import logoImg from '../../assets/logo.svg'
+
+function Profile() {
+    return (
+       <div className="register-container">
+          <h1>Profile</h1>
+       </div> 
+    );
+}
+
+export default Profile;
---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './styles.css'
+import logoImg from '../../assets/logo.svg'
+
+function Register() {
+    return (
+       <div className="register-container">
+          <div className="content">
+              <section>
+                <img src={logoImg} alt="Logo"/>
+                <h1>Register</h1>
+                <p> Make your registration, enter the platform and help people find the cases of your NGO </p>
+                <Link className="back-link" to="/">
+                       <FiArrowLeft size={16}  color="#e02041"/>
+                       Register
+                </Link>
+              </section>
+              <form>
+                <input type="text" placeholder="NGO Name"/>
+                <input type="email" placeholder="Email"/>
+                <input type="text" placeholder="Whatsapp"/>
+                <div className="input-group">
+                    <input type="text" placeholder="City"/>
+                    <input type="text" placeholder="UF" style={{width:80}}/>
+                </div>
+
+                <button className="button" type="submit">Register</button>
+                
+              </form>
+          </div>
+       </div> 
+    );
+}
+
+export default Register;
