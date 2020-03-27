5f18ae1045b5b472d564a8a10f0640a54c389223
carlosziegler <carlos.ziegler@gmail.com>
2020-03-27 18:38:01 +0100

    #5
    add Validation with CelebrateJs in routes

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,19 +1,58 @@
 const express = require('express')
-const routes = express.Router()
+const { celebrate, Segments , Joi } = require('celebrate')
+
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
 const ProfileController = require('./controllers/ProfileController')
 const SessionsController = require('./controllers/SessionController')
 
-routes.post('/sessions', SessionsController.store)
+const routes = express.Router()
 
-routes.post('/ongs', OngController.store)
+routes.post('/sessions', celebrate({
+    [Segments.BODY] : Joi.object().keys({
+        id: Joi.string().required(),
+    })
+})  , SessionsController.store)
+
 routes.get('/ongs', OngController.index)
 
-routes.get('/profile', ProfileController.index)
+routes.post('/ongs', celebrate({
+    [Segments.BODY] : Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}) ,OngController.store)
 
-routes.post('/incidents', IncidentController.store)
-routes.get('/incidents', IncidentController.index)
-routes.delete('/incidents/:id', IncidentController.delete)
+routes.get('/profile',celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}) , ProfileController.index)
+
+routes.post('/incidents', celebrate({
+    [Segments.BODY] : Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number().required(),
+    }),
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}) , IncidentController.store)
+
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), IncidentController.index)
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object({
+        id: Joi.number().required()
+    })
+}), IncidentController.delete)
 
 module.exports = routes
